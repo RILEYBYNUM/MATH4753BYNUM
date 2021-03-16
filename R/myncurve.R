cdc00b7e6d6eae51c5454fae5112ccb09118dd04
Riley Bynum <rileybynum25@ou.edu>
2021-03-16 16:25:14 -0500

    myncurve

---
+++ R/myncurve.R
@@ -0,0 +1,21 @@
+#' @tile Normal Probability Distribution
+#'
+#' @param mu mean
+#' @param sigma standard deviation
+#' @param a probablitiy bound
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{myncurve(mu = 4, sigma = 5, a = 3)}
+myncurve = function(mu, sigma, a){
+  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu+3*sigma))
+  #Shading the region
+  x1 = seq(-1, a, length = 100000)
+  y1 = dnorm(x1, mu, sigma)
+  polygon(c(-1, x1, a), c(0, y1, 0), col = "RED")
+  #Calculating the area
+  area1 = round(pnorm(a, mu, sigma), 4)
+
+}
