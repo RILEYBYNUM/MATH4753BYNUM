3f1e156bb4aaeb3df1a831371009ad6bcebbfbcc
Riley Bynum <rileybynum25@ou.edu>
2021-03-22 14:54:00 -0500

    adding fire data

---
+++ R/fire.R
@@ -0,0 +1,12 @@
+#' Fire distance and damage.
+#'
+#' A dataset containing the distance a fire is from a fire station and how much damge it caused.
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{distance}{distance a fire is from a fire station, in miles}
+#'   \item{damage}{x 1000 dollars in damage}
+#'   ...
+#' }
+#' @source \url{http://www.crcpress.com/Statistics-for-Engineering-and-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"fire"
